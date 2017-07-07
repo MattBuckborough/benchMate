756943b4fca5ffda8a440bf95a83864c759cae51
MattBuckborough <MattBuckborough@gmail.com>
2017-07-07 14:58:48 -0300

    Authentication now uses emails and hashes from db

--- app/models/Users.js
+++ app/models/Users.js
@@ -1,15 +1,24 @@
+// grab the mongoose module
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// create a Schema
+var userSchema = new Schema({
+	email : {type : String, index : true},
+	hash : Number
+});
 
+// create a model
+var User = mongoose.model('User', userSchema);
+User.create(function (err){
+	if (err) throw err;
+})
 
-exports.authenticate = function (user) {
-    console.log(user)
-    var testUsers = [{name: "tester", hash: -1014020185}];
+exports.authenticate = function (user,cb) {
     var status = false;
-    for (var i = 0; i < testUsers.length; i++){
-        if(user.name == testUsers[i].name && user.hash == testUsers[i].hash) {
-            status = true;
-            break;
-        }
-    }
-    return status;
+    User.find({}, function(err,users) {
+		if (err) return cb(err);
+        //console.log(users)
+        cb(null,users);
+	})
 
 }
