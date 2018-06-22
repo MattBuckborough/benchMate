bdedd04987aa4eb9c9d041ec767aaad73f632a10
mattbuckborough <mbuckborough@eastlink.ca>
2018-06-21 20:00:02 -0600

    gzip

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var mongoose        = require('mongoose');
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
 var bcrypt          = require('bcrypt');
-
+var compression     = require('compression');
 
 // configuration ===========================================
 	
@@ -20,6 +20,9 @@ mongoose.connect(db.url, {useMongoClient: true}); // connect to our mongoDB data
 app.use(bodyParser.json()); // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+
+//GZIP
+app.use(compression());
 
 /********AUTHENICATION*******/
 app.use(session({
